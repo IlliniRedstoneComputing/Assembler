28966e7723d9ee967e23464992eda4ac066f26d8
Jetorific <jtrom@illinois.edu>
2026-03-09 23:42:52 -0500

    updated interpreter, can accept .asm files now

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,7 +1,10 @@
 from sys import argv
 from os import path
+import os
 from pathlib import Path
 import readchar
+import subprocess
+import shutil
 
 class Interpreter:
     def __init__(self, data):
@@ -14,33 +17,34 @@ class Interpreter:
     def step(self):
         instr = self.data[self.pc]
 
+        condition_met = False
         match instr >> 6:
             case 0b00:
-                self.execute_branch(instr)
+                condition_met = self.execute_branch(instr)
             case 0b01:
                 self.execute_calculate(instr)
             case 0b10:
                 self.execute_copy(instr)
             case 0b11:
                 self.execute_immediate(instr)
-        self.pc += 1
+        
+        if not condition_met:
+            self.pc += 1
 
         if self.pc > 255:
             print("Program counter out of bounds\nProgram exited.")
             self.halted = True
 
     def execute_immediate(self, instr):
-
         data = instr & 0b00111111
         self.registers[0] = data
 
         if self.print_debug:
             print(f"[DEBUG-IMM ] PC: {self.pc:02x} | Instruction: {instr:08b} | Registers: {[f'{r:02x}' for r in self.registers]}. Load: {data:08b}")
-
+        
         return
 
     def execute_calculate(self, instr):
-
         operation = instr & 0b00000111
 
         match operation:
@@ -60,19 +64,24 @@ class Interpreter:
                 self.halted = True
 
         if self.print_debug:
-            print(f"[DEBUG-CALC] PC: {self.pc:02x} | Instruction: {instr:08b} | Registers: {[f'{r:02x}' for r in self.registers]}. Operation: {operation:03b}, Result: {self.registers[0]:08b}")
-
+            operations = ["OR", "NAND", "NOR", "AND", "ADD", "SUB", "HALT"]
+            print(f"[DEBUG-CALC] PC: {self.pc:02x} | Instruction: {instr:08b} | Registers: {[f'{r:02x}' for r in self.registers]}. Operation: {operations[operation]}, Result: {self.registers[3]:08b}")
+        
         return
     
     def execute_copy(self, instr):
         src = (instr & 0b00111000) >> 3
         dst = instr & 0b00000111
+        src_data = self.registers[src]
+
+        if self.print_debug:
+            print(f"[DEBUG-COPY] PC: {self.pc:02x} | Instruction: {instr:08b} | Registers: {[f'{r:02x}' for r in self.registers]}. Src: {'r'+str(src) if src < 7 else 'in'}, Dst: {'r'+str(dst) if dst < 7 else 'out'}, Data: {src_data:08b}")
 
         if dst == 0b111:
-            print(chr(self.registers[src]), end="", flush=True)
+            print(chr(self.registers[src]), end="\n" if self.print_debug else "", flush=True)
+            
             return
-
-        src_data = self.registers[src]
+        
         if (src == 0b111):
             char_read = readchar.readchar()
             # check if char_read is control c and if so, halt the program
@@ -80,19 +89,21 @@ class Interpreter:
                 self.halted = True
                 print("\nProgram exited with keyboard interrupt.")
                 return
-            src_data= ord(char_read)
+            src_data = ord(char_read)
         self.registers[dst] = src_data
 
-        if self.print_debug:
-            print(f"[DEBUG-COPY] PC: {self.pc:02x} | Instruction: {instr:08b} | Registers: {[f'{r:02x}' for r in self.registers]}. Src: r{src}, Dst: r{dst}, Data: {src_data:08b}")
         return
     
     def execute_branch(self, instr):
+        branch_address = self.registers[0]
         condition = instr & 0b00000111
+        data = self.registers[3]
+        
+        if self.print_debug:
+            print(f"[DEBUG-JUMP] PC: {self.pc:02x} | Instruction: {instr:08b} | Registers: {[f'{r:02x}' for r in self.registers]}. Condition: {condition:03b}, Data: {data:08b}, Goto: {branch_address:03b}, Jumped: {self.pc == self.registers[0]}")
     
         condition_met = False
 
-        data = self.registers[3]
         is_negative = data & 0b10000000 != 0
         is_zero = data == 0
         is_positive = not is_negative and not is_zero
@@ -104,11 +115,9 @@ class Interpreter:
             condition_met |= is_positive
 
         if condition_met:
-            self.pc = self.registers[0] - 1
+            self.pc = branch_address
 
-        if self.print_debug:
-            print(f"[DEBUG-JUMP] PC: {self.pc:02x} | Instruction: {instr:08b} | Registers: {[f'{r:02x}' for r in self.registers]}. Condition: {condition:03b}, Data: {data:08b}, Goto: {self.registers[0]:03b}, Jumped: {self.pc == self.registers[0]}")
-        return
+        return condition_met
 
     
 
@@ -117,13 +126,18 @@ def main():
         file_path = argv[1]
     else:
         file_path = input(".bin file to interpret: ")
-    if not file_path.endswith(".bin"):
+    
+    if file_path.endswith(".asm"):
+        customasm_path = shutil.which("customasm.exe")
+        data = subprocess.run([customasm_path, file_path, "-f", "hexcomma", "-q", "-p"], stderr=subprocess.STDOUT, shell=False, check=False, stdout=subprocess.PIPE).stdout
+        data = b''.join([int(str(byte).replace("'", "").replace("\\n","").replace("0x","")[1:],16).to_bytes(1) for byte in data.split(b', ')])
+    elif not file_path.endswith(".bin"):
         print("File must be .bin\nProgram exited.")
         return 1
-
-    with open(file_path, "rb") as f:
-        data = f.read()
-    # print(data.hex(sep="\t", bytes_per_sep=1))
+    else:
+        with open(file_path, "rb") as f:
+            data = f.read()
+        # print(data.hex(sep="\t", bytes_per_sep=1))
 
     interpreter = Interpreter(data)
 
